e456b86ad5ed2a1fc4d2b5f7345d233b81aba65f
karlisdaga <159028488+karlisdaga@users.noreply.github.com>
2024-03-15 13:19:46 +0100

    ESM_model.py

---
+++ PLM_models/ESM_model.py
@@ -0,0 +1,259 @@
+from transformers import AutoTokenizer, EsmModel, EsmForMaskedLM
+import torch
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from esm import pretrained
+import pickle as pkl
+import os
+import sys
+import scipy
+
+sys.path.append("../scripts")
+from utils import get_pseudo_likelihood
+
+class ESM():
+
+    """
+    Class for the protein Language Model
+    """
+
+    def __init__(self, method = "average", file_name = "."):
+        
+        """
+        Creates the instance of the language model instance, loads tokenizer and model
+
+        parameters
+        ----------
+
+        method: `str`
+        Which token to use to extract embeddings of the last layer
+        
+        file_name: `str`
+        The name of the folder to store the embeddings
+        """
+        # CACHE_DIR = "/cluster/scratch/asamant/models--facebook--esm1b_t33_650M_UR50S/snapshots/7b37824baec4d3658e1df7479222a7c79b465b76"
+        torch.cuda.empty_cache()
+
+        self.name_ = "esm1b_t33_650M_UR50S"
+        self.tokenizer = AutoTokenizer.from_pretrained("facebook/esm1b_t33_650M_UR50S")
+
+        self.method = method
+        self.file = file_name
+        self.repr_layer_ = -1
+
+        self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        model, alphabet = pretrained.load_model_and_alphabet(self.name_)
+        model.eval()
+        if torch.cuda.is_available():
+            model = model.cuda()
+        # model and alphabet
+        self.model_ = model
+        self.alphabet_ = alphabet
+
+        self.model = EsmModel.from_pretrained("facebook/esm1b_t33_650M_UR50S").to(self.device)
+
+        self.mask_model = EsmForMaskedLM.from_pretrained("facebook/esm1b_t33_650M_UR50S").to(self.device)
+        
+
+    def fit_transform(self, sequences:list, starts, ends, batches = 10):
+        """
+        Fits the model and outputs the embeddings.
+        
+        parameters
+        ----------
+
+        sequences: `list` 
+        List with sequences to be transformed
+        
+        batches: `int`
+        Number of batches. Per batch a checkpoint file will be saved
+        ------
+
+        None, saved the embeddings in the embeddings.csv
+        """
+        batch_size = round(len(sequences)/batches)
+        print("\nUsing the {} method".format(self.method))
+        
+        pooler_zero = np.zeros((len(sequences),1280))
+        for sequence,_ in zip(enumerate(sequences), tqdm(range(len(sequences)))):
+            if not isinstance(sequence[1], float):
+                j = sequence[0]
+                amino_acids = list(sequence[1])
+                seq_tokens = ' '.join(amino_acids)
+                tokenized_sequences = self.tokenizer(seq_tokens, return_tensors= 'pt') #return tensors using pytorch
+                tokenized_sequences = tokenized_sequences.to(self.device)
+                output = self.model(**tokenized_sequences)
+
+                if self.method == "average":
+                    output = torch.mean(output.last_hidden_state[:,starts[j]:ends[j],:], axis = 1)[0]
+                
+                elif self.method == "pooler":
+                    output = output.pooler_output[0]
+                
+                elif self.method == "last":
+                    output = output.last_hidden_state[0,ends[j]-1,:]
+
+                elif self.method == "first":
+                    output = output.last_hidden_state[0,starts[j],:]
+                    
+                pooler_zero[sequence[0],:] = output.detach().cpu().numpy()
+                # if sequence[0] % (batch_size+1) == 0:   #Checkpoint save
+                #     pd.DataFrame(pooler_zero).to_csv("outfiles/"+self.file+"/embeddings.csv")
+
+        # pd.DataFrame(pooler_zero).to_csv("outfiles/"+self.file+"/embeddings.csv")
+        return pd.DataFrame(pooler_zero,columns=[f"dim_{i}" for i in range(pooler_zero.shape[1])])
+
+    def calc_evo_likelihood_matrix_per_position(self, sequences:list, batch_size = 10):
+
+        batch_converter = self.alphabet_.get_batch_converter()
+        data = []
+        for i,sequence in enumerate(sequences):
+            data.append(("protein{}".format(i),sequence))
+        probs = []
+        count = 0
+        #One sequence at a time
+        for sequence,_ in zip(data,tqdm(range(len(data)))):
+            _, _, batch_tokens = batch_converter([sequence])
+            batch_tokens = batch_tokens.to("cuda:0" if torch.cuda.is_available() else "cpu")
+            out = self.model_(batch_tokens,repr_layers = [self.repr_layer_],return_contacts = False)
+            #Retrieve numerical values for each possible token (including aminoacids and special tokens) in each position
+            logits = out["logits"][0].cpu().detach().numpy()
+            #Turn them into probabilties 
+            prob = scipy.special.softmax(logits,axis = 1)
+            #Preprocessing probabilities, removing CLS and SEP tokens and removing probabilities of Special aminoacids and tokens of the model.
+            df = pd.DataFrame(prob, columns = self.alphabet_.all_toks)
+            df = df.iloc[:,4:-4]
+            df = df.loc[:, df.columns.isin(["U","Z","O","B","X"]) == False]
+            #removing CLS and SEP
+            df = df.iloc[1:-1,:]
+            df = df.reindex(sorted(df.columns), axis=1)
+            probs.append(df)
+
+            count += 1
+
+#Generate likelihoods for probs and sequences
+        likelihoods = get_pseudo_likelihood(probs, sequences)
+        pkl.dump([probs, likelihoods], open("/hpc/dla_lti/kdagakrumins/anaconda3/PLM_anamay/probabilities/probabilities_pseudo.pkl", "wb"))
+        print("done with predictions")
+
+        with open("/hpc/dla_lti/kdagakrumins/anaconda3/PLM_anamay/probabilities/probabilities_pseudo.pkl", "rb") as f:
+            data = pkl.load(f)
+#Make an empty Dataframe of probs_concatenated
+        probs_concatenated = pd.DataFrame()
+
+    # Iterate over each sequence and its corresponding DataFrame
+        for i, df in enumerate(probs):
+        # Add a blank row as a separator between sequences
+            separator_row = pd.DataFrame(index=[f"Sequence {i + 1}"], columns=df.columns)
+            probs_concatenated = pd.concat([probs_concatenated, separator_row, df])
+
+    # Reset the index of the concatenated DataFrame
+        probs_concatenated.reset_index(drop=True, inplace=True)
+        prob_by_column = {}
+        for column in probs_concatenated.columns:
+           prob_by_column[column] = probs_concatenated[column]
+
+# Concatenate the probabilities for each amino acid into a single DataFrame
+        prob_by_column_concatenated = pd.concat(prob_by_column, axis=1)
+
+
+        probs_concatenated.to_csv("/hpc/dla_lti/kdagakrumins/anaconda3/PLM_anamay/probabilities/probabilities_pseudo_ESM.csv",index=False)
+
+        best_sequences = []
+
+        for i, df in enumerate(probs):
+    # Iterate over each position in the transposed DataFrame
+            best_sequence = ""
+            for _, row in df.iterrows():
+        # Find the amino acid with the highest probability
+                best_amino_acid = row.idxmax()
+        
+        # Append the best amino acid to the best sequence
+                best_amino_acid = str(best_amino_acid)
+                best_sequence += best_amino_acid
+            best_sequences.append(best_sequence)
+    # Append the best sequence for the current sequence to the list
+# Print or use best_sequences as needed
+        print(best_sequences)
+
+        print("Done with predictions")
+        print("Done with predictions")
+        print(type(probs_concatenated))
+        self.best_sequences = best_sequences
+
+
+        return df, probs, best_sequences
+
+
+    def calc_pseudo_likelihood_sequence(self, sequences:list,starts, ends):
+
+        pll_all_sequences = []
+        self.mask_model = self.mask_model.to(self.device)
+
+        for j,sequence in enumerate(tqdm(sequences)):
+            try: 
+                amino_acids = list(sequence)
+                seq_tokens = ' '.join(amino_acids)
+                seq_tokens = self.tokenizer(seq_tokens, return_tensors='pt')
+                seq_tokens = seq_tokens.to(self.device)
+                logits = self.mask_model(**seq_tokens).logits[0].cpu().detach().numpy()
+                prob = scipy.special.softmax(logits,axis = 1)
+                df = pd.DataFrame(prob, columns = self.tokenizer.convert_ids_to_tokens(range(0,33)))
+                df = df.iloc[1:-1,:]
+
+                per_position_ll = []
+                for i in range(starts[j],ends[j]):
+                    aa_i = amino_acids[i]
+                    ll_i = np.log(df.iloc[i,:][aa_i])
+                    per_position_ll.append(ll_i)
+                
+                pll_seq = np.average(per_position_ll)
+                pll_all_sequences.append(pll_seq)
+            except:
+                pll_all_sequences.append(None)
+
+        return pll_all_sequences
+    
+    def calc_probability_matrix(self,sequence:str):
+
+        amino_acids = list(sequence)
+        seq_tokens = ' '.join(amino_acids)
+        seq_tokens = self.tokenizer(seq_tokens, return_tensors='pt')
+        seq_tokens = seq_tokens.to(self.device)
+        logits = self.mask_model(**seq_tokens).logits[0].cpu().detach().numpy()
+        prob = scipy.special.softmax(logits,axis = 1)
+        df = pd.DataFrame(prob, columns = self.tokenizer.convert_ids_to_tokens(range(0,33)))
+        df = df.iloc[1:-1,:]
+
+        return df
+
+    def process_sequences(self, sequences: list, starts,ends):
+    # Calculate evolutionary likelihoods for each sequence
+        likelihoods = self.calc_pseudo_likelihood_sequence(sequences, starts, ends)
+
+    # Get the list of best sequences
+        best_sequences = self.best_sequences
+        best_starts = [0] * len(best_sequences)
+        best_ends = [len(seq) for seq in best_sequences]
+   
+    # Update starts and ends with the values from best sequences
+        starts = best_starts
+        ends = best_ends
+
+    # Calculate pseudo likelihoods for each best sequence
+        pseudo_likelihoods = self.calc_pseudo_likelihood_sequence(best_sequences, starts, ends)
+        print(sequences,likelihoods,best_sequences,pseudo_likelihoods)
+    # Ensure all arrays have the same length
+        if len(sequences) != len(starts) or len(sequences) != len(ends):
+            raise ValueError("Lengths of sequences, starts, and ends must be equal.")
+
+    # Assuming best_sequences is a list of sequences
+    # Create a DataFrame to store the results
+        df_result = pd.DataFrame(columns=['Original_sequence', 'Evo_likelihood_original', 'Best_sequence', 'Pseudo_likelihood_best'])
+
+    # Use enumerate and zip to iterate over sequences, likelihoods, best_sequences, and pseudo_likelihoods together
+        for i, (seq, likelihood, best_seq, pseudo_likelihood) in enumerate(zip(sequences, likelihoods, best_sequences, pseudo_likelihoods)):
+            df_result.loc[i] = [seq, likelihood, best_seq, pseudo_likelihood]
+
+        return df_result
