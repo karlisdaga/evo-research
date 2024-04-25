ee233f159e2bc849736e4f46dc9ea84c67925a61
karlisdaga <159028488+karlisdaga@users.noreply.github.com>
2024-04-25 15:06:47 +0200

    Update embeddings_calculator.py

--- scripts/embeddings_calculator.py
+++ scripts/embeddings_calculator.py
@@ -36,30 +36,34 @@ if not os.path.exists(save_path):
 starts = [0]*repertoire_file.shape[0]
 ends = repertoire_file["full_sequence"].apply(len)
 
-for i,model in enumerate(init_list): ### the class method "fit_transform" calculates the embedding of each sequence
+
+    for i,model in enumerate(init_list): ### the class method "fit_transform" calculates the embedding of each sequence
      
         # if os.path.exists(os.path.join(save_path,f"embeddings_{suffixes[i]}.csv")):
         #     continue
 
-    if suffixes[i] == "ablang":
-        model = model(chain = "heavy")
-    elif suffixes[i] == "sapiens":
-        model = model(chain_type = "H")
-        sequences = repertoire_file["full_sequence"]
-        per_pos = model.generate_probability_matrix_csv(sequences)
-        best_evo = model.process_sequences(sequences=list(repertoire_file["full_sequence"]),starts=list(starts),ends=list(ends))
-        best_evo.to_csv(os.path.join(save_path,f"best_sequence_evo_sapiens.csv"), index = False)
- #      n_mut_sequences = model.mutate_sequences(num_mutations)   ### n_mut_sequences gives an iterated mutated sequence based on n amount of mutations(has to run all the previous functions)
- #      n_mut_sequences.to_csv(os.path.join(save_path,f"n_{num_mutations}_mut_sequences_sapiens.csv"), index = False)
-    else:
-        model = model()
+        if suffixes[i] == "ablang":
+            model = model(chain = "light")
+        elif suffixes[i] == "sapiens":
+            model = model(chain_type = "L")
+            sequences = repertoire_file["LC_CDR3"]
+            per_pos = model.generate_probability_matrix_csv(sequences)
+            evo_lhood = model.calc_pseudo_likelihood_sequence(sequences=list(repertoire_file["LC_CDR3"]),starts=list(starts),ends=list(ends))
+            evo_lhood.to_csv(os.path.join(save_path,f"best_sequence_evo_sapiens.csv"), index = False)
+          #  n_mut_sequences = model.mutate_sequences(num_mutations)
+          #  n_mut_sequences.to_csv(os.path.join(save_path,f"n_{num_mutations}_mut_sequences_sapiens.csv"), index = False)
+        else:
+            model = model()
 
-    embeds = model.fit_transform(sequences=list(repertoire_file["full_sequence"]),starts=list(starts),ends=list(ends))
-    embeds = pd.concat([repertoire_file,embeds],axis=1)
-    embeds.to_csv(os.path.join(save_path,f"embeddings_{suffixes[i]}.csv"), index=False)
-    if suffixes[i] == "ablang" or suffixes[i] == "ESM" or suffixes[i] == "protbert":
-        per_pos = model.calc_evo_likelihood_matrix_per_position(sequences=list(repertoire_file["full_sequence"]))
-        best_evo = model.process_sequences(sequences=list(repertoire_file["full_sequence"]),starts=list(starts),ends=list(ends))
-        best_evo.to_csv(os.path.join(save_path,f"best_sequence_evo_{suffixes[i]}.csv"), index = False)
-#        n_mut_sequences = model.mutate_sequences(num_mutations)
-#        n_mut_sequences.to_csv(os.path.join(save_path,f"n_{num_mutations}_mut_sequences_{suffixes[i]}.csv"), index = False)
+        embeds = model.fit_transform(sequences=list(repertoire_file["LC_CDR3"]),starts=list(starts),ends=list(ends))
+        embeds = pd.concat([repertoire_file,embeds],axis=1)
+        embeds.to_csv(os.path.join(save_path,f"embeddings_{suffixes[i]}.csv"), index=False)
+        if suffixes[i] == "ablang" or suffixes[i] == "ESM" or suffixes[i] == "protbert":
+            per_pos = model.calc_evo_likelihood_matrix_per_position(sequences=list(repertoire_file["LC_CDR3"]))
+            evo_lhood = model.calc_pseudo_likelihood_sequence(sequences=list(repertoire_file["LC_CDR3"]),starts=list(starts),ends=list(ends))
+            evo_lhood.to_csv(os.path.join(save_path,f"best_sequence_evo_{suffixes[i]}.csv"), index = False)
+#            n_mut_sequences = model.mutate_sequences(num_mutations)
+#            n_mut_sequences.to_csv(os.path.join(save_path,f"n_{num_mutations}_mut_sequences_{suffixes[i]}.csv"), index = False)
+#            delta_likelihood = model.delta_likelihood(sequences=list(repertoire_file["Mutated_sequence"]),starts=list(starts),ends=list(ends),num_mutations=num_mutations)
+#            delta_likelihood.to_csv(os.path.join(save_path,f"n_{num_mutations}_delta_likelihood_{suffixes[i]}.csv"), index = False)
+
