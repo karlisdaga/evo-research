9070bf84698d6456740062caa3782cabfc31f284
karlisdaga <159028488+karlisdaga@users.noreply.github.com>
2024-05-21 13:44:30 +0200

    Update ablang_model.py

--- PLM_models/ablang_model.py
+++ PLM_models/ablang_model.py
@@ -90,12 +90,6 @@ class Ablang():
 # Concatenate the probabilities for each amino acid into a single DataFrame
         prob_by_column_concatenated = pd.concat(prob_by_column, axis=1)
 
-
-#This works:
-        # Transpose the DataFrame so that each row represents a position and each column represents an amino acid
-        
-
-        # Reset the index so that the index represents the position
         output_dir = "probabilities"
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
@@ -110,9 +104,6 @@ class Ablang():
         best_sequences = []
 
         for i, df in enumerate(probs):
-    # Transpose the DataFrame so that each row represents a position and each column represents an amino acid
-    
-    # Iterate over each position in the transposed DataFrame
             best_sequence = ""
             for _, row in df.iterrows():
         # Find the amino acid with the highest probability
@@ -183,9 +174,7 @@ class Ablang():
     # Ensure all arrays have the same length
         if len(sequences) != len(starts) or len(sequences) != len(ends):
             raise ValueError("Lengths of sequences, starts, and ends must be equal.")
-
-    # Assuming best_sequences is a list of sequences
-    # Create a DataFrame to store the results
+            
         df_result = pd.DataFrame(columns=['Original_sequence', 'Evo_likelihood_original', 'Best_sequence', 'Pseudo_likelihood_best'])
 
     # Use enumerate and zip to iterate over sequences, likelihoods, best_sequences, and pseudo_likelihoods together
