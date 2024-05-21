4a86392444295c2c24367b8fd2cdf4be867df90b
karlisdaga <159028488+karlisdaga@users.noreply.github.com>
2024-05-21 10:59:43 +0200

    Update embeddings_calculator.py

--- scripts/embeddings_calculator.py
+++ scripts/embeddings_calculator.py
@@ -36,23 +36,23 @@ starts = [0]*repertoire_file.shape[0]
 ends = repertoire_file[column].apply(len)
 
 
-    for i,model in enumerate(init_list):
+for i,model in enumerate(init_list):
 
-        if suffixes[i] == "ablang":
-            model = model(chain = "heavy")
-        elif suffixes[i] == "sapiens":
-            model = model(chain_type = "H")
-            sequences = repertoire_file[column]
-            per_pos = model.generate_probability_matrix_csv(sequences)
-            evo_lhood = model.calc_pseudo_likelihood_sequence(sequences=list(repertoire_file[column]),starts=list(starts),ends=list(ends))
-            evo_lhood.to_csv(os.path.join(save_path,f"evo_likelihood_sapiens.csv"), index = False)
-        else:
-            model = model()
+    if suffixes[i] == "ablang":
+        model = model(chain = "heavy")
+    elif suffixes[i] == "sapiens":
+        model = model(chain_type = "H")
+        sequences = repertoire_file[column]
+        per_pos = model.generate_probability_matrix_csv(sequences)
+        evo_lhood = model.calc_pseudo_likelihood_sequence(sequences=list(repertoire_file[column]),starts=list(starts),ends=list(ends))
+        evo_lhood.to_csv(os.path.join(save_path,f"evo_likelihood_sapiens.csv"), index = False)
+    else:
+        model = model()
 
-        embeds = model.fit_transform(sequences=list(repertoire_file[column]),starts=list(starts),ends=list(ends))
-        embeds = pd.concat([repertoire_file,embeds],axis=1)
-        embeds.to_csv(os.path.join(save_path,f"embeddings_{suffixes[i]}.csv"), index=False)
-        if suffixes[i] == "ablang" or suffixes[i] == "ESM" or suffixes[i] == "protbert":
-            per_pos = model.calc_evo_likelihood_matrix_per_position(sequences=list(repertoire_file[column]))
-            evo_lhood = model.calc_pseudo_likelihood_sequence(sequences=list(repertoire_file[column]),starts=list(starts),ends=list(ends))
-            evo_lhood.to_csv(os.path.join(save_path,f"evo_likelihood_{suffixes[i]}.csv"), index = False)
+    embeds = model.fit_transform(sequences=list(repertoire_file[column]),starts=list(starts),ends=list(ends))
+    embeds = pd.concat([repertoire_file,embeds],axis=1)
+    embeds.to_csv(os.path.join(save_path,f"embeddings_{suffixes[i]}.csv"), index=False)
+    if suffixes[i] == "ablang" or suffixes[i] == "ESM" or suffixes[i] == "protbert":
+        per_pos = model.calc_evo_likelihood_matrix_per_position(sequences=list(repertoire_file[column]))
+        evo_lhood = model.calc_pseudo_likelihood_sequence(sequences=list(repertoire_file[column]),starts=list(starts),ends=list(ends))
+        evo_lhood.to_csv(os.path.join(save_path,f"evo_likelihood_{suffixes[i]}.csv"), index = False)
